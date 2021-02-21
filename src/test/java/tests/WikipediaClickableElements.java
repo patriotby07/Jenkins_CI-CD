239ea6e9a64b54877a93ec06883cd1e39fd43483
crushpowerx <asovin.e.v@gmail.com>
2021-02-21 15:37:10 +0200

    upd

--- src/test/java/tests/WikipediaClickableElements.java
+++ src/test/java/tests/WikipediaClickableElements.java
@@ -1,9 +1,18 @@
 package tests;
 
+import common.test_data.Links;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.BasePage;
 
+import static com.codeborne.selenide.Selenide.open;
+
 public class WikipediaClickableElements extends BasePage {
+
+    @BeforeMethod(alwaysRun = true)
+    public void openMainPage() {
+        open(Links.WIKIPEDIA_MAIN_PAGE.getUrl());
+    }
 
     @Test
     public void checkClickableElements() {
