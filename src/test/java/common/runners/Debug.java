a5db7cca728aaa22544a5a133ae4647f6a74d266
crushpowerx <asovin.e.v@gmail.com>
2021-03-07 18:26:57 +0200

    upd

--- src/test/java/common/runners/Debug.java
+++ src/test/java/common/runners/Debug.java
@@ -3,11 +3,11 @@ package common.runners;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeClass;
 
 public class Debug {
 
-    @BeforeMethod(alwaysRun = true)
+    @BeforeClass(alwaysRun = true)
     public void setUp() {
         Configuration.timeout = 30000;
         Configuration.startMaximized = true;
