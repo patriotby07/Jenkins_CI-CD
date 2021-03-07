d27f263a8fb1cc568aee322991280d0843b6bbfe
crushpowerx <asovin.e.v@gmail.com>
2021-03-07 19:27:26 +0200

    upd

--- src/test/java/config/SelenoidConfig.java
+++ src/test/java/config/SelenoidConfig.java
@@ -8,18 +8,11 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import static logger.CustomLogger.logger;
 
 public class SelenoidConfig {
-    public void createWebDriverInstance(String browser) {
-        if (browser.equals("${BROWSER}")) {
-            browser = "Chrome";
-        }
-        if (browser.equals("Chrome")) {
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("--disable-notifications");
-            Configuration.browserCapabilities.setCapability(ChromeOptions.CAPABILITY, options);
-            Configuration.browserVersion = "88";
-        }
-        System.out.println(browser);
-        Configuration.browser = browser;
+    public void createWebDriverInstance() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-notifications");
+        Configuration.browserCapabilities.setCapability(ChromeOptions.CAPABILITY, options);
+        Configuration.browserVersion = "88";
         Configuration.remote = "http://localhost:4444/wd/hub";
         Configuration.timeout = 30000;
         Configuration.reportsFolder = "target/screenshots";
--- src/test/java/runners/SelenoidRunner.java
+++ src/test/java/runners/SelenoidRunner.java
@@ -4,17 +4,13 @@ import com.codeborne.selenide.Selenide;
 import config.SelenoidConfig;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Optional;
-import org.testng.annotations.Parameters;
 
 public class SelenoidRunner {
 
-    @Parameters({"BROWSER"})
     @BeforeClass(alwaysRun = true)
-    public void setUp(@Optional("Chrome") String browser) {
-        System.out.println(browser);
+    public void setUp() {
         SelenoidConfig selenoidConfig = new SelenoidConfig();
-        selenoidConfig.createWebDriverInstance(browser);
+        selenoidConfig.createWebDriverInstance();
     }
 
     @AfterMethod(alwaysRun = true)
